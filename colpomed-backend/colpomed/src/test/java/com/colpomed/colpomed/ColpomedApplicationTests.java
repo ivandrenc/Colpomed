05c8732f7b55a17fd4c820ce01b9bf2fbe5fc0dc
Ivan Naranjo <ivannaranjo@pop-os.localdomain>
2023-08-27 16:55:06 +0200

    First Setup

---
+++ colpomed-backend/colpomed/src/test/java/com/colpomed/colpomed/ColpomedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colpomed.colpomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColpomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
